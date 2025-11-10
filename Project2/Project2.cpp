401af830ebe879c2c3c77a00026e9e8baef466ca
Дарина Сова <Daryna.Sova@cs.khpi.edu.ua>
2025-11-10 19:00:45 +0200

    w

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -9,8 +9,6 @@ int main() {
     int n;
     int finish = 0;
 
-    //comment
-
     cout << "-------------------------------------------- \n"
         << "|            Software engineering          | \n"
         << "|            Laboratory work 2             | \n"
